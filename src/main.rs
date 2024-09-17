f2237786ebb11b1ca5b4bdf2b4bf6c660ca1b622
Andrey Novikov <andreynovikov@MacBook-Pro-Andrey.local>
2024-09-18 04:46:06 +0500

    home work

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,45 @@
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    // Создание двух каналов
+    // Создание каналов
     let (tx1, rx1) = mpsc::channel();
     let (tx2, rx2) = mpsc::channel();
 
-    // Запуск потока для генерации чисел
-    let producer_thread = thread::spawn(move || {
-        for i in 1..=10 { // Инициализируем числа от 1 до 10
-            tx1.send(i).expect("Failed to send data");
-            thread::sleep(Duration::from_millis(500)); // Добавляем паузу
+    // Обертка для блокировки
+    let rx1 = Arc::new(Mutex::new(rx1));
+    let rx2 = Arc::new(Mutex::new(rx2));
+
+    // Поток для генерации чисел с паузами
+    let tx1_clone = tx1.clone();
+    thread::spawn(move || {
+        for i in 1..=10 {
+            tx1_clone.send(i).unwrap();
+            thread::sleep(Duration::from_millis(500)); // Пауза 500 мс
         }
     });
 
-    // Запуск потока для обработки чисел (возведение в квадрат)
-    let processor_thread = thread::spawn(move || {
-        for num in rx1 {
-            let squared = num * num;
-            tx2.send(squared).expect("Failed to send data");
+    // Поток для вычисления квадратов чисел
+    let tx2_clone = tx2.clone();
+    let rx1_clone = Arc::clone(&rx1);
+    thread::spawn(move || {
+        while let Ok(num) = rx1_clone.lock().unwrap().recv() {
+            let square = num * num;
+            tx2_clone.send(square).unwrap();
         }
     });
 
-    // Запуск потока для вывода результатов
-    let consumer_thread = thread::spawn(move || {
-        for squared in rx2 {
-            println!("{}", squared);
+    // Поток для вывода квадратов чисел
+    let rx2_clone = Arc::clone(&rx2);
+    thread::spawn(move || {
+        while let Ok(square) = rx2_clone.lock().unwrap().recv() {
+            println!("Квадрат числа: {}", square);
         }
     });
 
-    // Ожидание завершения всех потоков
-    producer_thread.join().expect("Producer thread panicked");
-    processor_thread.join().expect("Processor thread panicked");
-    consumer_thread.join().expect("Consumer thread panicked");
+    // Основной поток ожидает завершения работы всех потоков
+    // Поскольку у нас нет механизма для ожидания завершения потоков,
+    // основной поток просто будет спать, чтобы дать время другим потокам завершиться.
+    thread::sleep(Duration::from_secs(10));
 }
